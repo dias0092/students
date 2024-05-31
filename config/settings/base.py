05b8070f06dab6dbf54d53168ba27b892d1f3969
Dias <muhdias25@gmail.com>
2024-05-31 16:34:33 +0500

    added static files

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,3 +1,4 @@
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -127,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
