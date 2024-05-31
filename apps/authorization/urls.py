0e3c9e77835e8c126cd6baf1b310485db71d56a5
Dias <muhdias25@gmail.com>
2024-05-31 10:31:19 +0500

    added endpoints for change password and avatar

--- apps/authorization/urls.py
+++ apps/authorization/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from apps.authorization.views import LoginAPIView, UserDetailAPIView
+from apps.authorization.views import LoginAPIView, UserDetailAPIView, ChangeAvatarAPIView, ChangePasswordAPIView
 
 
 urlpatterns = [
     path('login/', LoginAPIView.as_view(), name='login'),
     path('user/', UserDetailAPIView.as_view(), name='user-detail'),
+    path('change-avatar/', ChangeAvatarAPIView.as_view(), name='change-avatar'),
+    path('change-password/', ChangePasswordAPIView.as_view(), name='change-password'),
 ]
--- apps/authorization/views.py
+++ apps/authorization/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, update_session_auth_hash
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
@@ -33,3 +33,42 @@ class UserDetailAPIView(APIView):
         user = request.user
         user_data = UserSerializer(user).data
         return Response(user_data, status=status.HTTP_200_OK)
+
+
+class ChangeAvatarAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        user_profile = request.user.userprofile
+        avatar = request.FILES.get('avatar')
+
+        if not avatar:
+            return Response({'error': 'No avatar file provided'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user_profile.avatar = avatar
+        user_profile.save()
+
+        return Response({'message': 'Avatar updated successfully', 'avatar': user_profile.avatar.url},
+                        status=status.HTTP_200_OK)
+
+
+
+class ChangePasswordAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        user = request.user
+        current_password = request.data.get('current_password')
+        new_password = request.data.get('new_password')
+
+        if not user.check_password(current_password):
+            return Response({'error': 'Current password is incorrect'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if current_password == new_password:
+            return Response({'error': 'New password cannot be the same as the current password'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user.set_password(new_password)
+        user.save()
+        update_session_auth_hash(request, user)  # Important to keep the user logged in after password change
+
+        return Response({'message': 'Password changed successfully'}, status=status.HTTP_200_OK)
