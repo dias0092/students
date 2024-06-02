1899eb70f8e5ff573382b7063e7365423e0d4969
Dias <muhdias25@gmail.com>
2024-06-02 21:05:11 +0500

    d

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,12 +8,9 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
-from config import settings
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.base')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=settings.STATIC_ROOT)
