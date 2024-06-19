99b24cec4ca2e9e36a7cfdf69cf5d444a0ad682e
Dias <muhdias25@gmail.com>
2024-06-19 17:07:50 +0500

    d

--- apps/studyplan/views.py
+++ apps/studyplan/views.py
@@ -231,7 +231,7 @@ class SimilarSubjectsAPIView(APIView):
             for user_subject in user_subjects:
                 for other_subject in other_university_subjects:
                     similarity = fuzz.token_set_ratio(user_subject.description, other_subject.description)
-                    if similarity >= 1:
+                    if similarity >= 0:
                         subject_semester = other_subject.subjectsemester_set.filter(semester__term=term, semester__year=year).first()
                         if subject_semester:
                             similar_subjects.append({
